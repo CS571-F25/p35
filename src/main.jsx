3c21cc548224ba3ae81e6a80e14e86b79f432708
Alex Tran <alexttran25@gmail.com>
2025-11-18 08:46:14 -0600

    Fix routing for GitHub Pages /p35 base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/p35">
     <App />
   </BrowserRouter>
 )
