cdd06536cc4fff2d844f9d378f819ecc2a56e253
Alex Tran <alexttran25@gmail.com>
2025-12-10 17:57:57 -0600

    Changed routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/p35">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
